b3681794175956f38fb5aca9d1ef387d692fdec1
Benoît Hubert <benoithubert@gmail.com>
2020-10-13 14:53:47 +0200

    Transmit data to child components via props

--- src/App.js
+++ src/App.js
@@ -11,9 +11,22 @@ function App() {
       <div className="album py-5 bg-light">
         <div className="container">
           <div className="row">
-            <AlbumCard />
-            <AlbumCard />
-            <AlbumCard />
+            <AlbumCard
+              text="Lorem ipsum 1ere card"
+              imageUrl="https://images.unsplash.com/photo-1593874236570-fd37dc57f23e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80"
+              links={{
+                view: 'http://example.com/1st-card/view',
+                edit: 'http://example.com/1st-card/edit',
+              }}
+            />
+            <AlbumCard
+              imageUrl="https://images.unsplash.com/photo-1547001817-11e7fa50ec93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            />
+            <AlbumCard
+              highlightTime
+              text="Lorem ipsum 3eme card"
+              imageUrl="https://images.unsplash.com/photo-1560755572-8b0fb6f23272?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            />
           </div>
         </div>
       </div>
--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
-function AlbumCard() {
+function AlbumCard(props) {
   return (
     <div className="col-md-4">
       <div className="card mb-4 shadow-sm">
-        <img className="bd-placeholder-img card-img-top" src="https://via.placeholder.com/150" alt="Placeholder" />
+        <img className="bd-placeholder-img card-img-top" src={props.imageUrl} alt="Placeholder" />
         <div className="card-body">
-          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p className="card-text">{props.text}</p>
           <div className="d-flex justify-content-between align-items-center">
             <div className="btn-group">
-              <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-              <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+              <a type="button" className="btn btn-sm btn-outline-secondary" href={props.links.view}>View</a>
+              <a type="button" className="btn btn-sm btn-outline-secondary" href={props.links.edit}>Edit</a>
             </div>
-            <small className="text-muted">9 mins</small>
+            <small className={props.highlightTime ? "text-danger" : "text-muted"}>9 mins</small>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+AlbumCard.defaultProps = {
+  imageUrl: 'https://via.placeholder.com/150',
+  links: {}
+};
 
 export default AlbumCard;
